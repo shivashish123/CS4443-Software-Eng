169231fa1eeec34ed80e59e93efbe07d4ff6f402
atharva-sarage <atharva.sarage@gmail.com>
2021-04-02 11:09:20 +0530

    package structure

---
+++ CS4443-CourseProject/src/main/java/com/lms/packages/Cs4443CourseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lms.packages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4443CourseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs4443CourseProjectApplication.class, args);
+	}
+
+}
---
+++ CS4443-CourseProject/src/test/java/com/lms/packages/Cs4443CourseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.packages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs4443CourseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
