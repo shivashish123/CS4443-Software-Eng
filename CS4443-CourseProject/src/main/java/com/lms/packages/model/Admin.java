79565ec321d07559e44ac445c5c1480dcb3f7f17
atharva-sarage <atharva.sarage@gmail.com>
2021-04-02 22:25:25 +0530

    User and Admin model class added

---
+++ CS4443-CourseProject/src/main/java/com/lms/packages/model/Admin.java
@@ -0,0 +1,39 @@
+package com.lms.packages.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="ADMIN")
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	// email address max length is 254
+	@Column(name="ADMIN_LOGIN_ID",length=254, nullable=false, unique=true)
+	private String loginId;
+	
+	@Column(name="ADMIN_NAME", length=50, nullable=false, unique=false)
+	private String name;
+	
+	@Temporal(TemporalType.DATE)
+    private Date birthDate;
+	
+	@Column(name="CONTACT_NO", length=12, nullable=true, unique=false)
+	private String contantNo;	
+	
+	@Column(name="ADDRESS", length= 100 ,nullable=true, unique=false)
+	private String address;	
+	
+	
+}
