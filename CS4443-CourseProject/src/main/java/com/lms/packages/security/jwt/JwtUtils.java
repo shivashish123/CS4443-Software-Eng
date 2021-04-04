6adf57aa9fd204b729cb755c8805d2f2c8362e28
atharva-sarage <atharva.sarage@gmail.com>
2021-04-04 10:49:14 +0530

    application properties

--- CS4443-CourseProject/src/main/java/com/lms/packages/security/jwt/JwtUtils.java
+++ CS4443-CourseProject/src/main/java/com/lms/packages/security/jwt/JwtUtils.java
@@ -15,10 +15,10 @@ import io.jsonwebtoken.*;
 public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
 
-	@Value("${bezkoder.app.jwtSecret}")
+	@Value("${lms.app.jwtSecret}")
 	private String jwtSecret;
 
-	@Value("${bezkoder.app.jwtExpirationMs}")
+	@Value("${lms.app.jwtExpirationMs}")
 	private int jwtExpirationMs;
 
 	public String generateJwtToken(Authentication authentication) {
