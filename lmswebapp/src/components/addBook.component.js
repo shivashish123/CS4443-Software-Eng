81aefe6585171412462ef021d2b561b261dd0f08
atharva-sarage <atharva.sarage@gmail.com>
2021-04-08 01:38:40 +0530

    add book ui add/remove , bug in remove

--- lmswebapp/src/components/addBook.component.js
+++ lmswebapp/src/components/addBook.component.js
@@ -20,9 +20,15 @@ export default class AddBook extends Component {
         this.onChangeTitle = this.onChangeTitle.bind(this)
         this.onChangeName = this.onChangeName.bind(this)
         this.onAddItem = this.onAddItem.bind(this)
+        this.onRemoveItem = this.onRemoveItem.bind(this)
+        this.onChangePublisher = this.onChangePublisher.bind(this)
+        this.onChangeCopies = this.onChangeCopies.bind(this)
         this.state = {
            title : "" ,
-           list : [{ name :"mike" },{ name :"virat" }]
+           list : [{ name :" " }],
+           publisher: "",
+           copies: 0 ,
+           temp :""
           };
           
     }
@@ -32,14 +38,25 @@ export default class AddBook extends Component {
           title: e.target.value
         });
     }
+    onChangeCopies(e) {
+        this.setState({
+          copies: e.target.value
+        });
+    }
+    onChangePublisher(e) {
+        this.setState({
+          publisher: e.target.value
+        });
+    }
 
-    onChangeName(e,idx) {  
-        console.log(idx)
-        const list = [...this.state.list];
-        list[idx]["name"] =  e.target.value;
-        return{
-            list
-        };
+    onChangeName(e,idx) {     
+        this.setState(state => {     
+            const list = [...state.list];
+            list[idx]["name"] =  e.target.value;
+            return{
+                list
+            };
+        });       
     }
     onAddItem = () => {
         this.setState(state => {
@@ -47,8 +64,21 @@ export default class AddBook extends Component {
           return {
             list
           };
-        });
-    };
+        })
+    }; 
+
+    onRemoveItem = (e,idx)=>{
+        this.setState(state => {
+            //var list = state.list.filter((item, j) => idx !== j);      
+            var list = [...state.list];
+            list.splice(idx, 1);
+            return {
+              list
+            };
+          });  
+          
+    }
+    
     handleSubmit(e){
         e.preventDefault();
         AddBookService.addBook()
@@ -75,12 +105,13 @@ export default class AddBook extends Component {
                         validations={[required]}
                         />
                     </div>
+                        <label htmlFor="Author">Author</label>
                         {this.state.list.map((x, i) => (
 
                             <div class="flexbox1">
                                 <Input
                                 type="text"
-                                className="form-control"
+                                className="form-control"                                
                                 name="Author"
                                 value={x.name}   
                                 onChange={(e)=>this.onChangeName(e,i)}                        
@@ -88,16 +119,41 @@ export default class AddBook extends Component {
                                 />    
                                 
                                 <div >
-                                    {i === 0 && <button type="button" onClick={this.onAddItem}>Add</button>}
-                                    {i !== 0 && <button type="button" class="hidden" >Add</button>}
+                                    {this.state.list.length !== 1 && <button type="button"  onClick={(e)=>this.onRemoveItem(e,i)}>X</button>}                                    
+                                    {this.state.list.length === 1 && <button type="button" class="hidden" >X</button>}
+                                    {i === (this.state.list.length-1) && <button type="button" onClick={this.onAddItem}>Add</button>}
+                                    {i !== (this.state.list.length-1) && <button type="button" class="hidden" >Add</button>}
                                
                                 </div>
                             </div>
 
                         ))}
+                         <div className="form-group">
+                            <label htmlFor="Publisher">Publisher</label>
+                            <Input
+                            type="text"
+                            className="form-control"
+                            name="Publisher"
+                            value={this.state.publisher}
+                            onChange={this.onChangePublisher}
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="Copies">Copies</label>
+                            <Input
+                            type="number"
+                            className="form-control"
+                            name="Copies"
+                            value={this.state.copies}
+                            onChange={this.onChangeCopies}
+                            />
+                        </div>                        
+
                     <button type="submit">Add Book</button>
                     </Form>
                     <div style={{ marginTop: 20 }}>{JSON.stringify(this.state.list)}</div>
+                    <div style={{ marginTop: 20 }}>{JSON.stringify(this.state.copies)}</div>
                 </div>
               </div>
         );
